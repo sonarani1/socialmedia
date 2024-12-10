4a5e9d0c51e94fd0ae58839a7047c8b8bdf1a73b
Sona Rani <110620118+sonarani1@users.noreply.github.com>
2024-12-10 22:39:53 +0530

    Update users.js

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,68 @@
 import User from "../models/User.js";
 
 /* READ */
+
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  // const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+  //   folder: "avatars",
+  //   width: 150,
+  //   crop: "scale",
+  // });
+
+  const { name, email, password,role} = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  
+    avatar: {
+      public_id: "sample",
+      url: "url",
+    },
+  });
+
+  sendToken(user, 201, res);
+});
+
+// Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // checking if user has given password and email both
+
+  if (!email || !password) {
+    return next(new ErrorHander("Please Enter Email & Password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHander("Invalid email or password", 401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHander("Invalid email or password", 401));
+  }
+
+  sendToken(user, 200, res);
+});
+
+// Logout User
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
+});
 export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
