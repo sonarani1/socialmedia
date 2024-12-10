735128857dbc8b4e2bef04de7faab18d777b2264
Sona Rani <110620118+sonarani1@users.noreply.github.com>
2024-12-10 20:38:23 +0530

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { getFeedPosts, getUserPosts, likePost,createPost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
+router.post("/post",createPost);
 /* READ */
 router.get("/", verifyToken, getFeedPosts);
 router.get("/:userId/posts", verifyToken, getUserPosts);
